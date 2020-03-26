bb66367fd22dc0c679f7c2f4224a39d9d86ae12b
Joshua Favre <jfavre390@gmail.com>
2020-03-25 20:37:30 -0600

    Began minor work on voxel terrain

---
+++ Assets/Scripts/WorldGenerator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour
+{
+    public GameObject voxel;
+    int cubicSize;
+
+    private void Start()
+    {
+        for (int x = 0; x < cubicSize; x++)
+            for (int y = 0; y < cubicSize; y++)
+            {
+
+            }
+    }
+}
