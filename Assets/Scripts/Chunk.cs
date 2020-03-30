4ffe2ad938ce4bbf6bb0e455f0d67b124fda6b14
Joshua Favre <jfavre390@gmail.com>
2020-03-29 22:58:33 -0400

    Finsihed chunk renderer

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -17,13 +17,12 @@ public class Chunk : MonoBehaviour
 
     private void Start()
     {
-        for (int y = 0; y < VoxelData.chunkHeight; y++)
-            for (int x = 0; x < VoxelData.chunkWidth; x++)
-                for (int z = 0; z < VoxelData.chunkWidth; z++)
-                    AddVoxelDataToChunk(new Vector3(x, y, z));
+        PopulateVoxelMap();
+        CreateMeshData();
 
         GenerateChunkMesh();
     }
+
 
     void PopulateVoxelMap()
     {
@@ -35,17 +34,43 @@ public class Chunk : MonoBehaviour
                 }
     }
 
+    void CreateMeshData()
+    {
+        for (int y = 0; y < VoxelData.chunkHeight; y++)
+            for (int x = 0; x < VoxelData.chunkWidth; x++)
+                for (int z = 0; z < VoxelData.chunkWidth; z++)
+                    AddVoxelDataToChunk(new Vector3(x, y, z));
+    }
+
+    bool CheckVoxel(Vector3 pos)
+    {
+        int x = Mathf.FloorToInt(pos.x), y = Mathf.FloorToInt(pos.y), z = Mathf.FloorToInt(pos.z);
+
+        if (x < 0 || x > VoxelData.chunkWidth - 1 || y < 0 || y > VoxelData.chunkHeight - 1 || z < 0 || z > VoxelData.chunkWidth - 1) return false;
+
+        return voxelMap[x, y, z];
+    }
+
     void AddVoxelDataToChunk(Vector3 position)
     {
         for (int x = 0; x < 6; x++)
-            for (int y = 0; y < 6; y++)
+            if (!CheckVoxel(position + VoxelData.faceChecks[x]))
             {
-                int triangleIndex = VoxelData.voxelTris[x, y];
-                vertices.Add(VoxelData.voxelVerts[triangleIndex] + position);
+                vertices.Add(position + VoxelData.voxelVerts[VoxelData.voxelTris[x, 0]]);
+                vertices.Add(position + VoxelData.voxelVerts[VoxelData.voxelTris[x, 1]]);
+                vertices.Add(position + VoxelData.voxelVerts[VoxelData.voxelTris[x, 2]]);
+                vertices.Add(position + VoxelData.voxelVerts[VoxelData.voxelTris[x, 3]]);
+                uvs.Add(VoxelData.voxelUvs[0]);
+                uvs.Add(VoxelData.voxelUvs[1]);
+                uvs.Add(VoxelData.voxelUvs[2]);
+                uvs.Add(VoxelData.voxelUvs[3]);
                 triangles.Add(vertexIndex);
-                vertexIndex++;
-
-                uvs.Add(VoxelData.voxelUvs[y]);
+                triangles.Add(vertexIndex + 1);
+                triangles.Add(vertexIndex + 2);
+                triangles.Add(vertexIndex + 2);
+                triangles.Add(vertexIndex + 1);
+                triangles.Add(vertexIndex + 3);
+                vertexIndex += 4;
             }
     }
 
--- Assets/Scripts/VoxelData.cs
+++ Assets/Scripts/VoxelData.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public static class VoxelData
 {
-    public static readonly int chunkWidth = 16;
-    public static readonly int chunkHeight = 256;
+    public static readonly int chunkWidth = 10;
+    public static readonly int chunkHeight = 50;
 
     public static readonly Vector3[] voxelVerts = new Vector3[8]
     {
@@ -31,12 +31,12 @@ public static class VoxelData
 
     public static readonly int[,] voxelTris = new int[,]
     {
-        { 0, 3, 1, 1, 3, 2 }, // Back face
-        { 5, 6, 4, 4, 6, 7 }, // Front face
-        { 3, 7, 2, 2, 7, 6 }, // Top face
-        { 1, 5, 0, 0, 5, 4 }, // Bottom face
-        { 4, 7, 0, 0, 7, 3 }, // Left face
-        { 1, 2, 5, 5, 2, 6 }, // Right face
+        { 0, 3, 1, 2 }, // Back face
+        { 5, 6, 4, 7 }, // Front face
+        { 3, 7, 2, 6 }, // Top face
+        { 1, 5, 0, 4 }, // Bottom face
+        { 4, 7, 0, 3 }, // Left face
+        { 1, 2, 5, 6 }, // Right face
     };
 
     public static readonly Vector2[] voxelUvs = new Vector2[]
@@ -44,8 +44,6 @@ public static class VoxelData
         new Vector2(0, 0),
         new Vector2(0, 1),
         new Vector2(1, 0),
-        new Vector2(1, 0),
-        new Vector2(0, 1),
         new Vector2(1, 1),
     };
 }
